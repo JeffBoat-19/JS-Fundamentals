dbf1bef6053dfd9667a316ddab88ba0e76cb0c75
Jeffery Boateng <jeff.boat.jb@gmail.com>
2025-07-12 03:18:21 +0000

    Added a function

--- 9-add.js
+++ 9-add.js
@@ -1,4 +1,6 @@
-function add(a, b) {
-  return a + b;
+const arg1 = Number(process.argv[2]);
+const arg2 = Number(process.argv[3]);
+function add(arg1, arg2) {
+  return arg1 + arg2;
 }
-console.log(add(1, 7));
+console.log(add(arg1, arg2));
