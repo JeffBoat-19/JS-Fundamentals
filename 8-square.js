11cfd04a08b57df54154e1538a3bd66d18101c16
Jeffery Boateng <jeff.boat.jb@gmail.com>
2025-07-12 02:57:53 +0000

    Calculated for an integer

---
+++ 8-square.js
@@ -0,0 +1,8 @@
+const arg = parseInt(process.argv[2]);
+if (isNaN(arg) || arg < 0) {
+  console.log("Missing size");
+} else {
+  for (i = 0; i < arg; i++) {
+    console.log("X".repeat(arg));
+  }
+}
